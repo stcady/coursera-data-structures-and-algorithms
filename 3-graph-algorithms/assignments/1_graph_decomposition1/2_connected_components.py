69ef2eec76a1fd7141f82eaab95642d19d9f7114
Steven Cady <stevencady@Stevens-Personal-MacBook.local>
2022-09-19 23:00:04 -0700

    graph decomp 1

---
+++ 3-graph-algorithms/assignments/1_graph_decomposition1/2_connected_components.py
@@ -0,0 +1,33 @@
+#Uses python3
+
+import sys
+
+
+def number_of_components(adj):
+    visited = [False] * len(adj)
+    marks = [-1] * len(adj)
+    mark = 0
+    for v in range(0, len(adj)):
+        if not visited[v]:
+            explore(adj, visited, marks, mark, v)
+            mark+=1
+    return mark
+
+def explore(adj, visited, marks, mark, s):
+    visited[s] = True
+    marks[s] = mark
+    for d in adj[s]:
+        if not visited[d]:
+            explore(adj, visited, marks, mark, d)
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    print(number_of_components(adj))
